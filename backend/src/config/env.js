a6e3c8c189cd268ab3b82c40e8c97b5733e1bd95
SplitVICE <justviceart@gmail.com>
2021-06-21 10:04:35 -0600

    Default port if not stated set to 4500
    Documentation updates regarding default port and examples of .env file

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,7 +1,7 @@
 // Create .env file /backend/.env
 require('dotenv').config();
 module.exports = {
-    PORT: process.env.PORT || 4000,
+    PORT: process.env.PORT || 4500,
     SHOW_FILES_STORED: process.env.SHOW_FILES_STORED || 'FALSE',
     EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET,
     USE_HTTPS: process.env.USE_HTTPS || 'FALSE',
